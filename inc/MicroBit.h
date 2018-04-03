f89ea1deb168c6c0fac6ec065e43a7b79a900eda
ewarrior <51904@qq.com>
2018-04-03 09:24:50 +0800

    remove macro

--- inc/MicroBit.h
+++ inc/MicroBit.h
@@ -111,12 +111,8 @@ class MicroBit
     MicroBitDisplay             display;
     MicroBitButton              buttonA;
     MicroBitButton              buttonB;
-#if MICROBIT_DISPLAY_TYPE == ARMBIT_V01
     MicroBitButton              buttonC;
     MicroBitButton              buttonD;
-#endif
-
-
     MicroBitMultiButton         buttonAB;
     MicroBitAccelerometer       accelerometer;
     MicroBitCompass             compass;
